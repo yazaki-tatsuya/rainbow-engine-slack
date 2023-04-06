59f24b80c2251b9abed06ab8e2ae60e0bce0dd4b
yazaki-tatsuya <yazaki.tatsuya.0813@gmail.com>
2023-04-06 23:59:59 +0900

    20230406_IT#0705_Slack_api_投稿取得

--- 20230406_IT#0705_Slack_api_投稿取得/slack_get_conversation_list.py
+++ 20230406_IT#0705_Slack_api_投稿取得/slack_get_conversation_list.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 file_path = Path(__file__).parent.parent
 # 「.env」ファイルのパスを設定
 env_path = file_path/'.env'
+print(env_path)
 # 「.env」ファイルのロード
 load_dotenv(dotenv_path=env_path)
 
@@ -16,16 +17,18 @@ def main():
 
     # APIのURL
     url = os.environ['URL']
+    channel = ("?channel="+os.environ['CHANNEL']) if os.environ['CHANNEL'] != "" else ""
+    url = url + channel
+    print(url)
 
     # ヘッダー部
     headers = {
-        "Content-Type":"application/json",
+        "Content-Type":"application/x-www-form-urlencoded",
         "User-Agent":"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)",
         "Authorization":"Bearer "+os.environ['SLACK_BOT_TOKEN']
     }
     
     # POSTリクエスト
-    # →ユーザー一覧を取得します。
     response = requests.post(url, headers=headers)
     
     # レスポンスの内容をtext形式で取得
@@ -38,12 +41,11 @@ def main():
     # 第１引数：json形式のオブジェクト
     # 第２引数：分解対象のデータのkey
     # 第３引数：その他、分解した明細に付与するデータ
-    df = pd.json_normalize(json_object, record_path =['members'])
+    df = pd.json_normalize(json_object, record_path =['messages'])
     # df = pd.json_normalize(json_object, record_path =['students'],meta=['school_name', 'class'])
  
     # 表形式データ（DataFrame）をcsv出力する
-    # →日本語も含まれるため、エンコーディングはshift-jisを指定
-    df.to_csv('./slack_get_user_list.csv', sep=',', encoding='shift-jis', header=True)
+    df.to_csv('./slack_get_conversation_history.csv', sep=',', encoding='shift-jis', header=True)
 
 if __name__ == "__main__":
      main()
